9b8e2b8f30ae11401d5fd00a218087260aa078ad
shubham <purkaitshubham5@gmail.com>
2025-08-16 15:55:48 +0530

    sam

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Task = require('./models/Task');
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
