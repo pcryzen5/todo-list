b17c9110c12abca94fd2739161ead7d5e98f00bf
shubham <purkaitshubham5@gmail.com>
2025-08-16 10:59:37 +0530

    Add cors dependency

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json()); // Parse incoming JSON
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://Shubham:shubham(56)@cluster0.nz6emhg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
